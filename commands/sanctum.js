936286f25f18b4c109b1c098fbccf340f382713e
Boatshoes24 <wmoulton4@gmail.com>
2022-01-01 21:08:41 -0500

    updates sanctum.json and sanctum.js to make it easier to duplicate over for future raids

--- commands/sanctum.js
+++ commands/sanctum.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js')
-const raidJSON = require('./sanctum.json')
-const NUM_BOSSES = 10;
+const { bosses, num_bosses } = require("./sanctum.json")
 const spaceChar = '\u200B'
 
 function formatEmbedArgs(args) {
@@ -11,11 +10,12 @@ function formatEmbedArgs(args) {
     }
     if (!args.length) {
         embedFields = []
-        raidJSON.forEach(item => {
-            if (item.category === 'info') {
-                embedFields.push({ name: item.name, value: item.value, inline: true })
-            }
-        })
+        let auraCount = 0
+        for (let bossIdx in bosses) {
+            let boss = bosses[bossIdx]
+            auraCount = Object.keys(boss.value).length
+            embedFields.push({ name: boss.name, value: `${boss.command}\nAuras: ${auraCount}`, inline: true })
+        }
         if (embedFields.length > 3) {
             let emptyFieldNum = 3 - (embedFields.length % 3)
             for (let i = 0; i < emptyFieldNum; i++) {
@@ -23,17 +23,18 @@ function formatEmbedArgs(args) {
             }
         }
     }
-    else if (index !== NaN && index === 0) {
+    else if (index !== NaN && index >= 0 && index <= num_bosses) {
         embedFields = []
-        raidJSON.forEach(item => {
-            if (item.category === 'auras' && item.boss_num === index) {
-                if (Object.keys(item.value).length === 0) {
-                    embedFields.push({ name: item.name, value: 'No Auras', inline: true })
+        for (let bossIdx in bosses) {
+            let boss = bosses[bossIdx]
+            if (boss.boss_num === index) {
+                if (Object.keys(boss.value).length === 0) {
+                    embedFields.push({ name: boss.name, value: 'No Auras', inline: false })
                 }
                 else {
-                    embedFields.push({ name: item.name, value: spaceChar, inline: false })
-                    for (let aura in item.value) {
-                        embedFields.push({ name: aura, value: `[Link](${item.value[aura]})`, inline: true})
+                    embedFields.push({ name: boss.name, value: spaceChar, inline: false })
+                    for (let aura in boss.value) {
+                        embedFields.push({ name: aura, value: `[Link](${boss.value[aura]})`, inline: true })
                     }
                     if (embedFields.length > 3) {
                         let emptyFieldNum = 3 - ((embedFields.length - 1) % 3)
@@ -42,30 +43,8 @@ function formatEmbedArgs(args) {
                         }       
                     }
                 }
-            }            
-        })
-    }
-    else if (index !== NaN && index > 0 && index <= NUM_BOSSES) {
-        embedFields = []
-        raidJSON.forEach(item => {
-            if (item.category === 'auras' && item.boss_num === index) {
-                if (Object.keys(item.value).length === 0) {
-                    embedFields.push({ name: item.name, value: 'No Auras', inline: true })
-                }
-                else {
-                    embedFields.push({ name: item.name, value: spaceChar, inline: false})
-                    for (let aura in item.value) {
-                        embedFields.push({ name: aura, value: `[Link](${item.value[aura]})`, inline: true })
-                    }
-                    if (embedFields.length > 3) {
-                        let emptyFieldNum = 3 - ((embedFields.length - 1) % 3)
-                        for (let i = 0; i < emptyFieldNum; i++) {
-                            embedFields.push({ name: spaceChar, value: spaceChar, inline: true })
-                        }       
-                    }
-                }                
             }
-        })
+        }
     }
     return embedFields
 }
@@ -74,15 +53,14 @@ function formatDescription(args) {
     let description = 'false'
     if (args[0] === 'all') {
         description = '**All Auras** \n\n'
-        raidJSON.forEach(item => {
-            if(item.category === 'auras') {
-                if (Object.keys(item.value).length > 0) {
-                    for (let aura in item.value) {
-                        description += `${item.name}: [${aura}](${item.value[aura]})\n`
-                    }
+        for (let bossIdx in bosses) {
+            let boss = bosses[bossIdx]
+            if (Object.keys(boss.value).length > 0) {
+                for (let aura in boss.value) {
+                    description += `${boss.name}: [${aura}](${boss.value[aura]}) \n`
                 }
             }
-        })
+        }
     }
     return description
 }
