d9a00ebbd79be58b52d3964b55e8f6b1cdd28dbe
Boatshoes24 <wmoulton4@gmail.com>
2023-04-05 17:47:19 -0400

    add shoda command

---
+++ commands/shoda.js
@@ -0,0 +1,10 @@
+
+module.exports ={
+    name: 'shoda',
+    args: false,
+    aliases: ['showda', 'showduh'],
+    cooldown: 30,
+    execute(msg, args) {
+        msg.channel.send('https://media.tenor.com/k2hXBG1xhcUAAAAd/tommy-boy-david-spade.gif')
+    }
+}
